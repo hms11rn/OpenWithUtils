233c29cb5af62e310c684f7e9046007834ec8942
hms11rn <hmselik@gmail.com>
2023-04-04 10:10:38 +0300

    added custom xplane .obj opener

--- OpenWithUtils/OpenWithUtils.cpp
+++ OpenWithUtils/OpenWithUtils.cpp
@@ -1,32 +1,35 @@
 ﻿// OpenWIthUtils.cpp : Defines the entry point for the application.
 //
-#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+ #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 
 #include "OpenWithUtils.h"
 #include "Windows.h"
-#include "shellapi.h"
 #include "winuser.h"
+#include "shellapi.h"
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
 
-int main(int argc, char* argv[]) {	
-	if (argc != 3) {
+int main(int argc, char* argv[]) {
+	if (argc < 3) {
 		cout << "exit1 not enough arguments" << endl;
-		return 0;
+		return 1;
 	}
 	string type = argv[2];
-	if (type != "cmd" && type != "cmdParent" && type!= "clipboardCopy") {
-		cout << "exit2" << endl;
-		return 0;
+	if (type != "cmd" && type != "cmdParent" && type!= "clipboardCopy" && type != "obj") {
+		cout << "exit2 incorrect type options are: [cmd, cmdParent, clipBoardCopy]" << endl;
+		return 1;
 	}
 
 	if (type == "cmd") {
 		string run = "/K cd \"";
-		run.append(argv[1]);
-		run.append("\" && cmd");
+		run = run + argv[1] + "\" && cmd";
 		const char* cstr = run.c_str();
 		ShellExecute(0, "open", "cmd", cstr, 0, SW_SHOW);
+		
+		
 		cout << "ran command - " << run << endl;
 		return 0;
 	} else if (type == "cmdParent") {
@@ -50,10 +53,60 @@ int main(int argc, char* argv[]) {
 		SetClipboardData(CF_TEXT, hMem);
 		CloseClipboard();
 	}
-	
+
+	// very specific to x-plane .obj (same file extension different formats, so I am using this program to run it, detect which kind of format it is, then run the apporpriate app for it.
+	else if (type == "obj") {
+		if (argc != 5) {
+			return 1;
+	}
+		
+		string openWithDef = argv[3];
+		string openWithXP = argv[4];
+		ifstream file(argv[1]);
+		string line1, line2, line3;
+		boolean xpObj = false; // default set to false.
+		if (file.is_open()) {
+			int i = 0;
+			boolean breakFlag = true;
+			while (file.good() && breakFlag) {
+				i++;
+				switch(i) {
+				case 1:
+					file >> line1;
+					break;
+				case 2:
+					file >> line2;
+					break;
+				case 3:
+					file >> line3;
+					breakFlag = false;
+					break;
+
+				}
+			}
+			cout << line1 << endl;
+			cout << line2 << endl;
+			cout << line3 << endl;
+
+			string location = argv[1];
+			string stRun = openWithDef + " \"" + location + "\""; // adding quotes to file path
+			openWithXP = "\"" + openWithXP + "\" " + location; // adding quotes to X-Plane Object viewer path
+			cout << stRun << endl;
+			if (line1.length() == 1 && line2 == "800" && line3 == "OBJ") {
+				cout << "Running X-Plane Object Viewer: " << openWithXP.c_str() << endl;
+				system(openWithXP.c_str());
+			}
+			else {
+				cout << "Regular Obj" << endl;
+				string location = argv[1];
+				string locationWQuotes = "\"" + location + "\"";
+				ReplaceStringInPlace(openWithDef, "%2", locationWQuotes); // %2 is just to replace location
+				system(openWithDef.c_str());
+			}
+			return 0;
+		}
+	}
 	return 0;
 }
 
-void copyToClipBoard(string source) {
-	
-}
+
